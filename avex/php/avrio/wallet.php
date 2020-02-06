dba313783d2124be98cfc1717212df7d1680fe4e
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-06 02:44:13 +0000

    Update wallet.php

--- avex/php/avrio/wallet.php
+++ avex/php/avrio/wallet.php
@@ -22,29 +22,36 @@ function getStatus() {
   return $response;
 }
 function addFunds(userId, ammount) {
-
+    return true;
 }
 
 function removeFunds(userId, ammount) {
-
+    return true;
 }
 function transferFund(sId, rId, ammount) {
-
+    return true;
 }
 function deleteWallet(userId) {
-
+    return true;
 }
 function withdrawFunds(userid, ammount, address) {
-
+    return true;
 }
 function getBalance(userId) {
-
+    return 3.2292
 }
 function getDepositAddress(userid) {
-
+    return "Aio88999cccf8a312b413e2e49d91ce91653f5f4be32159634818676afb78dc647" ;
 }
 function getTransactions(userid) {
-
+    $txns = [
+        0 => "288999cccf8a312b413e2e49d91ce91653f5f4be32159634818676afb78dc647",
+        1 => "9299accccf8a312b413e2e49d91ce91653f5f4be32159634818676afb78jsj82"
+    ];
+    return txns;
+}
+function lockFunds(userid,ammount) {
+    return true;
 }
 
 ?>
