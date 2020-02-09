88c779959395f213066dfeb06449c17a106918e3
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-09 01:37:41 +0000

    Update wallet.php

--- avex/php/avrio/wallet.php
+++ avex/php/avrio/wallet.php
@@ -1,57 +1,25 @@
 <?php
-use TurtleCoin\TurtleService;
+function get_wallet(userId,currency) //Balance, Locked funds, Deposit address {
+    $wallet = [
+    0 => 1.2345, // bal
+    1 => 0.032, // locked bal
+    2 => "Aio88999cccf8a312b413e2e49d91ce91653f5f4be32159634818676afb78dc647" // deposit address
+}
+    
+function lock_funds(userId,currency,direction,amount) //Direction being '+' or '-' {
+    return 1;
+}
+    
+function withdraw(userId,currency,amount,wallet_to,fee) {
+    return 1;
 
-$config = [
-    'rpcHost'     => 'http://localhost',
-    'rpcPort'     =>  12345,
-    'rpcPassword' => 'password',
-];
-
-$avrio-wallet = new TurtleService($config);
-function reset() {
-  $scanHeight = 0;
-  $response = $avrio-wallet->reset($scanHeight);
-  return $response;
 }
-function save() {
-  $response = $avrio-wallet->save();
-  return $response;
-}
-function getStatus() {
-  $response = $avrio-wallet->getStatus();
-  return $response;
-}
-function addFunds(userId, ammount) {
-    return true;
+    
+function create_wallets(new_userId) {
+    return 1;
 }
 
-function removeFunds(userId, ammount) {
-    return true;
+function transfer_funds(userIdFrom,userIdTo,currency,amount) {
+    return 1;
 }
-function transferFund(sId, rId, ammount) {
-    return true;
-}
-function deleteWallet(userId) {
-    return true;
-}
-function withdrawFunds(userid, ammount, address) {
-    return true;
-}
-function getBalance(userId) {
-    return 3.2292
-}
-function getDepositAddress(userid) {
-    return "Aio88999cccf8a312b413e2e49d91ce91653f5f4be32159634818676afb78dc647" ;
-}
-function getTransactions(userid) {
-    $txns = [
-        0 => "288999cccf8a312b413e2e49d91ce91653f5f4be32159634818676afb78dc647",
-        1 => "9299accccf8a312b413e2e49d91ce91653f5f4be32159634818676afb78jsj82"
-    ];
-    return txns;
-}
-function lockFunds(userid,ammount) {
-    return true;
-}
-
 ?>
