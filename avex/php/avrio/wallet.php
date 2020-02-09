42b9f8e33c7c758b2493f0c954b1af3ee6d36557
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-09 01:45:46 +0000

    Update wallet.php

--- avex/php/avrio/wallet.php
+++ avex/php/avrio/wallet.php
@@ -1,10 +1,18 @@
 <?php
 function get_wallet(userId,currency) //Balance, Locked funds, Deposit address {
-    $wallet = [
-        0 => 1.2345, // bal
-        1 => 0.032, // locked bal
-        2 => "Aio88999cccf8a312b413e2e49d91ce91653f5f4be32159634818676afb78dc647" // deposit address
-    ]
+    if (currency == "BTC") {
+        $wallet = array(
+            "balance" => 0.345834,
+            "locked_balance" => 0.0003,
+            "deposit_address" => "1999cccf8a312b413e2e49d91ce91653f5f4be32159634818676afb78dc647"
+         )
+    } else if (currency == "AIO") {
+        $wallet = array(
+            "balance" => 1.2345,
+            "locked_balance" => 0.032,
+            "deposit_address" => "Aio88999cccf8a312b413e2e49d91ce91653f5f4be32159634818676afb78dc647"
+        )
+    }
     return wallet;
 }
     
