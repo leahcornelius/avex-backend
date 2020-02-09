f12115a2ce4e80dcc0f7686ddd2a78356ae8b675
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-09 15:31:44 +0000

    Update and rename avex/php/avrio/wallet.php to avex/php/wallet.php

---
+++ avex/php/wallet.php
@@ -0,0 +1,41 @@
+<?php
+require_once '/API/jsonRPCClient.php';
+$bitcoin = new jsonRPCClient('http://username:password@127.0.0.1:8332/');
+function get_wallet(userId,currency) { //Balance, Locked funds, Deposit address 
+    if ($currency == "BTC") {
+        $address=$bitcoin->getaccountaddress($userId);
+        $wallet = array(
+            "balance" => bitcoind->getbalance($userId),
+            "locked_balance" => 0.0003,
+            "deposit_address" => $address
+         )
+    } else if ($currency == "AIO") {
+        $wallet = array(
+            "balance" => 104.3200,
+            "locked_balance" => 0.032,
+            "deposit_address" => "Aio88999cccf8a312b413e2e49d91ce91653f5f4be32159634818676afb78dc647"
+        )
+    }
+    return wallet;
+}
+    
+function lock_funds(userId,currency,direction,amount) //Direction being '+' or '-' {
+    return 1;
+}
+    
+function withdraw(userId,currency,amount,wallet_to,fee) {
+    if (strtoupper($currency) ==  "BTC") {
+        return $bitcoin->sendfrom($userId,$receiver,$amount,3);  
+    } else if (strtoupper($currency) == "AIO") {
+        return ; // todo
+    }
+}
+    
+function create_wallets(new_userId) {
+    return $bitcoin->getaccountaddress($new_userId);
+}
+
+function transfer_funds(userIdFrom,userIdTo,currency,amount) {
+    return 1;
+}
+?>
