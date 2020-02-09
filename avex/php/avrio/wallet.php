d3e9e0cbbe14874a319b3b97ac99141c0d4c1db2
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-09 01:39:36 +0000

    Update wallet.php

--- avex/php/avrio/wallet.php
+++ avex/php/avrio/wallet.php
@@ -1,9 +1,10 @@
 <?php
 function get_wallet(userId,currency) //Balance, Locked funds, Deposit address {
     $wallet = [
-    0 => 1.2345, // bal
-    1 => 0.032, // locked bal
-    2 => "Aio88999cccf8a312b413e2e49d91ce91653f5f4be32159634818676afb78dc647" // deposit address
+        0 => 1.2345, // bal
+        1 => 0.032, // locked bal
+        2 => "Aio88999cccf8a312b413e2e49d91ce91653f5f4be32159634818676afb78dc647" // deposit address
+    ]
 }
     
 function lock_funds(userId,currency,direction,amount) //Direction being '+' or '-' {
